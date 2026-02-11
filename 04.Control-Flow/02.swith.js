1756561ecf687aa43d1b41ae058f6cd09a0dd051
rony-sda <iammehedihasanrony@gmail.com>
2026-02-11 19:37:18 +0600

    execution context and callstack and control flow

---
+++ 04.Control-Flow/02.swith.js
@@ -0,0 +1,29 @@
+// syntex
+switch (key) {
+  case value:
+    break; // without break matched or after all code executed.
+
+  default:
+    break;
+}
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "march":
+    console.log("march");
+    break;
+  case "april":
+    console.log("april");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
