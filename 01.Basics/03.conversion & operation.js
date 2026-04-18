696cf02441d9955637ac17ea75ff2b40ea1d8e80
rony-sda <iammehedihasanrony@gmail.com>
2026-04-19 04:24:35 +0600

    promise and async

--- 01.Basics/03.conversion & operation.js
+++ 01.Basics/03.conversion & operation.js
@@ -1,9 +1,8 @@
-
 // *********************** Conversion ***********************
 
 // --------------- Number ------------------
-const score = '32'
-let valueInNumber = Number(score)
+const score = "32";
+let valueInNumber = Number(score);
 console.log(typeof valueInNumber); // number
 console.log(valueInNumber);
 
@@ -12,74 +11,68 @@ console.log(valueInNumber);
 // "33abc" => NaN
 //  true => 1; false => 0
 
-
 // --------------- Boolean ------------------
-let isLoggedIn = "Rony"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn); // true
+let isLoggedIn = "Rony";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); // true
 
 // Example
 // 1 => true; 0 => false
 // "" => false
 // "Rony" => true
 
-
 // --------------- String ------------------
-let someNumber = 33
-let stringNumber = String(someNumber)
+let someNumber = 33;
+let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber); // string
 
-
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 console.log(negValue); // -3
 
-console.log(2+2); // 4
-console.log(2-2); // 0
-console.log(2*2); // 4
-console.log(2**3); // 8
-console.log(2/3); // 0.6666666666666666
-console.log(2%3); // 2
+console.log(2 + 2); // 4
+console.log(2 - 2); // 0
+console.log(2 * 2); // 4
+console.log(2 ** 3); // 8
+console.log(2 / 3); // 0.6666666666666666
+console.log(2 % 3); // 2
 
-let str1 = "hello"
-let str2 = " Rony"
-let str3 = str1 + str2
+let str1 = "hello";
+let str2 = " Rony";
+let str3 = str1 + str2;
 console.log(str3); // "hello Rony"
 
 console.log("1" + 2); // "12"
 console.log(1 + "2"); // "12"
 console.log("1" + 2 + 2); // "122"
 console.log(1 + 2 + "2"); // "32"
-console.log( (3 + 4) * 5 % 3); // 2
+console.log(((3 + 4) * 5) % 3); // 2
 console.log(+true); // 1
 console.log(+""); // 0
-
 
 // --------------- prefix vs postfix ------------------
 
 // postfix
 let x = 5;
-console.log(x++);   // 5
-console.log(x);     // now 6
-console.log(a--);   // 5
-console.log(a);   // 4
+console.log(x++); // 5
+console.log(x); // now 6
+console.log(a--); // 5
+console.log(a); // 4
 
 // prefix
 let y = 5;
-console.log(++y);   // 6
-console.log(y);     // 6
-
+console.log(++y); // 6
+console.log(y); // 6
 
 // example
 let i = 5;
 let result = i++ + ++i;
 // result = 5 + 7 = 12
-console.log(result);   // 12
-console.log(i);        // 7
-
+console.log(result); // 12
+console.log(i); // 7
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ 06.Advance/01.Promise.js
@@ -0,0 +1,119 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  //Do an async task
+  // DB calls, cryptography, network
+  setTimeout(function () {
+    console.log("Async task is compelete");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Chai", email: "chai@example.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "hitesh", password: "123" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected"));
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E: ", error);
+  }
+}
+
+getAllUsers();
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
+
+// promise static methods
+// one fail = all fail ❌
+await Promise.all([fn1(), fn2(), fn3()]);
+
+// fail steel runing, all result  ✅
+await Promise.allSettled([fn1(), fn2(), fn3()]);
+
+// Race first complete than result 🏃
+await Promise.race([fn1(), fn2(), fn3()]);
+
+// timeout create
+await Promise.race([fetchData(), delay(3000)]);
+
+// one success — than result ✅
+// all fail than — fail ❌
+await Promise.any([fn1(), fn2(), fn3()]);
+
+// multiple server to data fetch
+await Promise.any([fetchFromServer1(), fetchFromServer2(), fetchFromServer3()]);
